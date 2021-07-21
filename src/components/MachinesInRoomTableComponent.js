b8da578acadeefa0ee34ad2779895e954e0860fe
Kazim Tecimer <ayberk.tecimer@tum.de>
2021-07-21 12:13:04 +0000

    update machine in room settings

--- src/components/MachinesInRoomTableComponent.js
+++ src/components/MachinesInRoomTableComponent.js
@@ -1,50 +1,62 @@
-import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
-import {IconButton, TableCell} from "@material-ui/core";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-function DeleteForeverIcon() {
-    return null;
+let laundryRoom =     {
+    "isActive": true,
+    "machines": [
+        "60f48d84240f7f049053bc1c"
+    ],
+    "_id": "60f48d5f240f7f049053b676",
+    "name": "name-1",
+    "address": "address-1",
+    "operationStartHour": 6,
+    "operationEndHour": 24,
+    "__v": 0
+};
+const useStyles = makeStyles({
+    table: {
+        minWidth: 650,
+    },
+});
+
+function createData(activity) {
+    return { activity};
 }
 
-function MachinesInRoomTableComponent(props) {
+const rows = [
+    createData(laundryRoom.name)
+];
 
-    const columns = [
-        { field: 'id', headerName: 'Machine # In Room', width: 200 },
-        { field: 'firstName', headerName: 'Price', width: 200},
-        { field: 'lastName', headerName: 'Enable', width: 200 },
-        {
-            field: 'age',
-            headerName: 'Add & Remove',
-            type: 'number',
-            width: 300,
-        }
-    ];
-
-    const rows = [
-        { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-        { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-        { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-        { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-        { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-        { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-        { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-        { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-        { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-    ];
-
+export default function BasicTable() {
+    const classes = useStyles();
+    console.log(rows);
     return (
-        <React.Fragment>
-        <div style={{ height: 600, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} pageSize={5} />
-        </div>
-            <TableCell align="right">
-                <IconButton
-                    color="inherit">
-                    <DeleteForeverIcon/>
-                </IconButton>
-            </TableCell>
-            </React.Fragment>
+        <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Activity</TableCell>
+
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow key={row.activity}>
+                            <TableCell component="th" scope="row">
+                                {row.activity}
+                            </TableCell>
+
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
-
-export default MachinesInRoomTableComponent;
