cf8218b15e2be856b526656e65b4edc71eeb5273
ahmettalhasen <talhasen@protonmail.com>
2021-07-23 17:55:52 +0200

    usage and revenue stats pages return the basic stats correctly

--- src/views/RevenueStatistics.js
+++ src/views/RevenueStatistics.js
@@ -1,32 +1,43 @@
 import {Grid, Paper} from "@material-ui/core";
 import { Helmet } from 'react-helmet';
-import RoomInfoComponent from "../components/RoomInfoComponent";
-import WorkingHoursComponent from "../components/WorkingHoursComponent";
-import AnnouncementsComponent from "../components/AnnouncementsComponent";
 import {connect, useSelector} from "react-redux";
 import {withRouter} from "react-router-dom";
 import React, {useEffect} from "react";
 import Loading from "../components/Loading";
+import {getStatistics} from "../redux/actions";
 
-function RevenueStatistics() {
+
+function RevenueStatistics(props) {
 
     const user = useSelector((state) => state.user);
+    const stats = useSelector((state) => state.statistics);
+    let {match, getStatistics} = props;
 
     useEffect(() => {
         // trigger room load from backend
+        getStatistics(user.user._id);
         console.log("GET IN TO REVENUE STATISTICS")
-        console.log(user);
+        console.log(stats);
     }, []);
 
 
-    return (!user ? <Loading/> :
+    return (!stats.value ? <Loading/> :
         <div>
             <Helmet>
                 <title>LaundryWise | Revenue Statistics</title>
             </Helmet>
-            <h1>
-                {user.user.username}
-            </h1>
+            <h3>
+                Total revenue machines have been used:
+                {stats.value.totalCount}
+            </h3>
+            <h3>
+                Total revenue washer machines have been used: 
+                {stats.value.washerCount}
+            </h3>
+            <h3>
+                Total revenue dryer machines have been used:
+                {stats.value.dryerCount}
+            </h3>
             <br/>
             <Paper>
                 <h1>Revenue Statistics</h1>
@@ -36,7 +47,6 @@ function RevenueStatistics() {
     );
 }
 
-// export default RevenueStatistics;
-export default connect()(RevenueStatistics);
-
-// export default connect()(withRouter(UserLoginView));
+export default connect(null, {getStatistics})(
+    RevenueStatistics
+);
