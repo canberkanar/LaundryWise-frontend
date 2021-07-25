91659f9628492d6fbde9cdc0c5e90be7859d639f
abcdrk <kdreyyan@gmail.com>
2021-07-25 23:27:00 +0200

    Removed useless movieApp files

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,5 +1,4 @@
 export * from "./userActions";
-export * from "./movieActions";
 export * from "./laundryroomActions"
 export * from "./roomActions"
 export * from "./machineActions"
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,5 @@
 import {combineReducers} from "redux";
 import user from "./userReducer";
-import entities from "./entitiesReducer";
 import selectedMovie from "./selectedMovieReducer";
 import {selectedLaundryRoom, allLaundryRooms, updatedRoom, myLaundryRooms} from "./laundryRoomReducer";
 import {selectRoom} from "./roomReducer";
@@ -14,7 +13,6 @@ import reserveReducer from "./reserveReducer";
 //reducers
 export default combineReducers({
     user,
-    entities,
     selectedMovie,
     selectedLaundryRoom,
     allLaundryRooms,
